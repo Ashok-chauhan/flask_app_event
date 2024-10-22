85069897af49bbd27d951e6befdef2be83363a99
Ashok-chauhan <ashok975@gmail.com>
2024-10-22 12:33:04 +0530

    changes in config

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ashok975'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URI')\
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URI')\
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABSE_URI')\
     or 'mysql+pymysql://flask:FlaskApp_9@localhost/events-x'
     # or 'mysql+pymysql://root:@localhost/eventsx'
     #or 'sqlite:///'+ os.path.join(basedir, 'app.db')
--- myapp.wsgi
+++ myapp.wsgi
@@ -1,10 +1,7 @@
 import sys
-from dotenv import load_dotenv
-
-
 sys.path.insert(0, '/var/www/flask_app_event')
 sys.path.insert(0, '/var/www/flask_app_event/myenv/lib/python3.10/site-packages')
-load_dotenv()
+
 from app import create_app
 application = create_app()
 
