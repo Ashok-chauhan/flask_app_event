25fded20c23819b2b18d0168d5859a17d23aeb92
Ashok-chauhan <ashok975@gmail.com>
2024-10-22 11:09:07 +0530

    .env_example added

--- config.py
+++ config.py
@@ -6,16 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ashok975'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URI')\
     or 'mysql+pymysql://flask:FlaskApp_9@localhost/events'
-    # or 'mysql+pymysql://root:@localhost/events'
-        # or 'mysql+pymysql://flask:FlaskApp_9@localhost/events'
-        # or 'mysql+pymysql://root:@localhost/events'
-    # or 'mysql+pymysql://root:@localhost/events'
-    
-    #or 'mysql+pymysql://root:@localhost/events'
+    # or 'mysql+pymysql://root:@localhost/eventsx'
     #or 'sqlite:///'+ os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_ENV ='production'
-    FLASK_DEBUG = False
     # UPLOAD_DIRECTORY = 'uploads/'
     # MAX_CONTENT_LENGTH = 16*1024*1024 #16 MB
     # ALLOWED_EXTENTIONS = ['.jpg', 'jpeg', '.png', '.gif', '.pdf']
