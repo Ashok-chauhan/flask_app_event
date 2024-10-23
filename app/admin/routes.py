cd230478f7e2517615abb12a53c3ab1d6ad1db5f
Ashok-chauhan <ashok975@gmail.com>
2024-10-23 17:09:32 +0530

    upload chnaged

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -21,10 +21,10 @@ def upload_file(file):
     try:
         extension = os.path.splitext(file.filename)[1].lower()
         if file:
-               #  if extension not in current_app.config['ALLOWED_EXTENTIONS']:
-               #      return 'File is not allowed.'
-               #  file.save(os.path.join(current_app.config['UPLOAD_DIRECTORY'], secure_filename(file.filename)))
-                file.save(os.path.join(current_app.config['UPLOAD_DIRECTORY'], file.filename))
+                if extension not in current_app.config['ALLOWED_EXTENTIONS']:
+                    return 'File is not allowed.'
+                file.save(os.path.join(current_app.config['UPLOAD_DIRECTORY'], secure_filename(file.filename)))
+               #  file.save(os.path.join(current_app.config['UPLOAD_DIRECTORY'], file.filename))
 
                 return secure_filename(file.filename)
     except:
